4639469b18f96beae269a289fa30527484ebf0b3
DNParadox <m96francesco@gmail.com>
2022-06-06 17:05:35 +0200

    Javascript quasi finito

--- js/script.js
+++ js/script.js
@@ -6,12 +6,21 @@ generateButton.addEventListener('click',
         const userKm = parseInt( document.getElementById('user-km').value);
         console.log(userKm)
         const userAge = document.getElementById('user-age').value;
-        console.log (userAge)
+        console.log(userAge)
 
 
         const price = userKm * 0.21;
         console.log (price)
+        let discount = 0;
 
+        if (userAge === 'minorenne') {
+            discount = price * 0.2
+        } else if (userAge === 'over') {
+            discount = price * 0.4
+        }
+
+        const finalprice = price - discount
+        console.log (finalprice)
 
     }
 )
